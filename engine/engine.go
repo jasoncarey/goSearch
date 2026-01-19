11ab54aa4cd3d7b99e555ed85c5d06f87abb6d01
Jason Carey <jasdcarey@gmail.com>
2026-01-20 00:12:08 +1100

    add bruteforce engine

---
+++ engine/engine.go
@@ -0,0 +1,34 @@
+package engine
+
+import (
+	"sort"
+
+	"search/vec"
+)
+
+type Data struct {
+	index  int
+	vector vec.Vector
+}
+
+type Result struct {
+	index int
+	score float64
+}
+
+type Engine struct {
+	data []Data
+}
+
+func (e *Engine) BruteForceSearch(query vec.Vector, k int) []Result {
+	// check query against everything in the data store
+	results := []Result{}
+	for i, d := range e.data {
+		score := query.CosineSimilarity(d.vector)
+		results = append(results, Result{index: i, score: score})
+	}
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].score > results[j].score
+	})
+	return results[:k]
+}
